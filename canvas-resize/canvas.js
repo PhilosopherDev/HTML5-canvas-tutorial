9c6e82118841234ff1d892e7d73f5010795c4691
PhilosopherProgrammer <nekarakube@gmail.com>
2022-07-30 23:29:58 +0900

    Draw line and arc

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -6,9 +6,38 @@ canvas.height = window.innerHeight;
 
 const c = canvas.getContext('2d');
 
+c.fillStyle = 'yellow';
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'red';
 c.fillRect(400, 100, 100, 100);
+c.fillStyle = 'cyan';
 c.fillRect(600, 200, 100, 100);
-c.fillRect(300, 500, 100, 100);
+c.fillStyle = 'purple';
+c.fillRect(300, 300, 100, 100);
+
+// 선 그리기
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.lineTo(50, 300);
+c.strokeStyle = 'skyblue';
+c.stroke();
+
+
+// 원 그리기
+c.beginPath();
+c.arc(300, 300, 50, 0, Math.PI * 2);
+c.strokeStyle = 'blue';
+c.stroke();
+
+for (let i = 0; i < 10; i++) {
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+    c.beginPath();
+    c.arc(x, y, 50, 0, Math.PI * 2);
+    c.strokeStyle = 'blue';
+    c.stroke();
+}
 
 console.log(canvas);
